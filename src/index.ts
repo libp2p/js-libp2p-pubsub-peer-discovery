0c36da36fe165533252a0f7574e5e37bf9687621
Marin Petrunic <marin.petrunic@gmail.com>
2022-11-29 16:37:22 +0100

    fix: libp2p components type

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ export interface PubsubPeerDiscoveryInit {
 
 export interface PubSubPeerDiscoveryComponents {
   peerId: PeerId
-  pubsub: PubSub
+  pubsub?: PubSub
   addressManager: AddressManager
 }
 
